0c8fa7d1676ddc361377dbf863955984a8d2becb
Husen00 <Husen.Muhsen@Student.HTW-Berlin.de>
2024-03-23 16:56:43 +0100

    Upload the Snake Game

--- Snake Game/src/main.cpp
+++ Snake Game/src/main.cpp
@@ -4,10 +4,17 @@
 #include <windows.h>
 using namespace std;
 
+void Shift_Right(int arr[], int size)
+{
+    for (int i = size - 2; i >= 0; i--)
+    {
+        arr[i + 1] = arr[i];
+    }
+}
 //-------------Properties--------------------------//
 enum Direction { Top =1 , Down, Right, Left };
 struct Map { int Width, Height, FruitX, FruitY; };
-struct Snake { int HeadX, HeadY, Tail_N; Direction Dir; };
+struct Snake { int HeadX, HeadY, Tail_N, Tail_X[50], Tail_Y[50]; Direction Dir; };
 struct Player { int Score; bool Lose; };
 //-------------------------------------------------//
 
@@ -41,11 +48,24 @@ void Draw()
     {
         for (int j = 0; j < Map1.Width; j++)
         {
-            if (i == 0 || i == Map1.Height - 1) cout << "*"; //in the first and last column
-            else if (j == 0 || j == Map1.Width - 1) cout << "*";
-            else if (i == Snake1.HeadY && j == Snake1.HeadX) cout << "O";
+            if (i == 0 || i == Map1.Height - 1) cout << "-"; //in the first and last column
+            else if (j == 0 || j == Map1.Width - 1) cout << "|";
+            else if (i == Snake1.HeadY && j == Snake1.HeadX) cout << "O"; //Head
             else if (i == Map1.FruitY && j == Map1.FruitX) cout << "$";
-            else cout << " ";
+            else 
+            {
+                bool printed = false;
+                for (int z = 0; z < Snake1.Tail_N; z++)
+                {
+                    if (Snake1.Tail_X[z] == j && Snake1.Tail_Y[z] == i)
+                    {
+                        cout << "o";
+                        printed = true;
+                        break;
+                    }
+                }
+                if (!printed) cout << " ";
+            }
 
         }
         cout << endl;
@@ -74,6 +94,11 @@ void input()
 
 void Move()
 {
+    Shift_Right(Snake1.Tail_X, 50);
+    Shift_Right(Snake1.Tail_Y, 50);
+    Snake1.Tail_X[0] = Snake1.HeadX;
+    Snake1.Tail_Y[0] = Snake1.HeadY;
+
     switch (Snake1.Dir)
     {
     case Top:Snake1.HeadY--; break;
@@ -102,7 +127,7 @@ int main()
         Draw();
         input();
         Move();
-        Sleep(50);
+        Sleep(90);
     }
     system("pause");
     return 0;
